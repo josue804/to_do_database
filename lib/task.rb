6edba2c9e800e6c57ae9ec906be9a03b61f1812d
Josue Valverde & Corrie Smith <student@epicodus.com>
2015-08-17 11:47:30 -0700

    Add and pass spec to sort tasks

--- lib/task.rb
+++ lib/task.rb
@@ -1,10 +1,11 @@
 class Task
   @@Task = []
-  attr_reader(:description, :list_id)
+  attr_reader(:description, :due_date, :list_id)
 
   define_method(:initialize) do |attributes|
     @description = attributes.fetch(:description)
-    @id = attributes.fetch(:id)
+    @due_date = attributes.fetch(:due_date)
+    @list_id = attributes.fetch(:list_id)
   end
 
   define_singleton_method(:all) do
@@ -12,17 +13,29 @@ class Task
     tasks = []
     results.each() do |result|
       description = result.fetch("description")
-      tasks.push(Task.new({:description => description, :id => nil}))
+      due_date = result.fetch("due_date")
+      tasks.push(Task.new({:description => description, :list_id => nil, :due_date => due_date}))
     end
     tasks
   end
 
   define_method(:save) do
-    result = DB.exec("INSERT INTO tasks (description) VALUES ('#{@description}') RETURNING id;")
+    result = DB.exec("INSERT INTO tasks (description) VALUES ('#{@description}') RETURNING list_id;")
   end
 
   define_method(:==) do |other|
     same_description = self.description().eql?(other.description())
     same_description
+  end
+
+  define_singleton_method(:sort) do
+    results = DB.exec("SELECT * FROM tasks ORDER BY due_date ASC;")
+    tasks = []
+    results.each() do |result|
+      description = result.fetch("description")
+      due_date = result.fetch("due_date")
+      tasks.push(Task.new({:description => description, :list_id => nil, :due_date => due_date}))
+    end
+    tasks
   end
 end
