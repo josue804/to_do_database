2486e7bf26047a4f91a8a34de59c6e0b11662488
Josue Valverde & Corrie Smith <student@epicodus.com>
2015-08-17 10:58:52 -0700

    Add and pass spec to return all

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,14 +1,4 @@
-require('rspec')
-require('pg')
-require('task')
-
-DB = PG.connect({:dbname => 'my_database_test'})
-
-RSpec.configure do |config|
-  config.after(:each) do
-    DB.exec("DELETE FROM tasks * ;")
-  end
-end
+require('spec_helper')
 
 describe(Task) do
 
