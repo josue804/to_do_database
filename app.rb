9717106219c4fa76b946ddb6105149510720df31
Ryan Heupler & Josue Valverde <student@epicodus.com>
2015-08-18 10:48:17 -0700

    add working task and list pages

--- app.rb
+++ app.rb
@@ -11,15 +11,31 @@ get('/') do
   erb(:index)
 end
 
-post('/lists') do
-  name = params.fetch('name')
+# get ('lists/new') do
+#   erb(:list_form)
+# end
+
+post('/') do
+  name = params.fetch('list_name')
   list = List.new({:name => name, :id => nil})
   list.save()
   @lists = List.all()
   erb(:index)
 end
 
-get('/list/:id') do
-  @list = List.find(params.fetch('id').to_i())
+get('/lists/:id') do
+  @list = List.all()
+  @id = params.fetch('id').to_i()
   erb(:list)
 end
+
+post('/lists/:id') do
+  description = params.fetch('task_description')
+  due_date = params.fetch('due_date')
+  @list_id = params.fetch('id').to_i()
+  task = Task.new({:description => description, :due_date => due_date, :list_id => @list_id})
+  task.save()
+  @tasks = Task.all()
+  erb(:list)
+end
+# get('tasks')
